821bc9288ecb0dcb9f8034e962e45e72494ce2e0
diamondz1975 <diamondz1975@c63c96d2-bf12-11de-a250-c7f789e21d83>
2009-10-29 16:11:47 +0000

    initial commit of WIP

---
+++ src/src/uk/co/bitethebullet/android/token/TokenList.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright Mark McAvoy - www.bitethebullet.co.uk 2009
+ * 
+ * This file is part of Android Token.
+ *
+ * Android Token is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Android Token is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Android Token.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package uk.co.bitethebullet.android.token;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * Main entry point into Android Token application
+ * 
+ * Generates OATH compliant HOTP or TOTP tokens which can be used
+ * in place of a hardware token.
+ * 
+ * For more information about this project visit
+ * http://code.google.com/p/androidtoken/
+ */
+public class TokenList extends ListActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
