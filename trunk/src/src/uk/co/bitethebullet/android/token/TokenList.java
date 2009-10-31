f81a4cfbc315efd7fc999f6502c3c21daca4ac25
diamondz1975 <diamondz1975@c63c96d2-bf12-11de-a250-c7f789e21d83>
2009-10-31 18:35:28 +0000

    wip on UI

--- trunk/src/src/uk/co/bitethebullet/android/token/TokenList.java
+++ trunk/src/src/uk/co/bitethebullet/android/token/TokenList.java
@@ -21,6 +21,10 @@ package uk.co.bitethebullet.android.token;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 /**
  * Main entry point into Android Token application
@@ -32,10 +36,59 @@ import android.os.Bundle;
  * http://code.google.com/p/androidtoken/
  */
 public class TokenList extends ListActivity {
+	
+	private static final int MENU_ADD_ID = Menu.FIRST;
+	private static final int MENU_PIN_CHANGE_ID = Menu.FIRST + 1;
+	private static final int MENU_PIN_REMOVE_ID = Menu.FIRST + 2;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);		
+		menu.add(0, MENU_ADD_ID, 0, R.string.menu_add);	
+		menu.add(0, MENU_PIN_CHANGE_ID, 1, R.string.menu_pin_change);
+		menu.add(0, MENU_PIN_REMOVE_ID, 2, R.string.menu_pin_remove);
+		return true;
+	}
+
+	
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()){
+		case MENU_ADD_ID:
+			createToken();
+			return true;
+			
+		case MENU_PIN_CHANGE_ID:
+			//TODO: MM to complete
+			return true;
+			
+		case MENU_PIN_REMOVE_ID:
+			//TODO: MM to complete
+			return true;
+		}
+		
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	/**
+	 * Starts the process of creating a new token in the application
+	 */
+	private void createToken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
 }
