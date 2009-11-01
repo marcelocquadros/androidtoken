47a0e53393cb07289977d3cc0d4d101725b794ba
diamondz1975 <diamondz1975@c63c96d2-bf12-11de-a250-c7f789e21d83>
2009-11-01 20:03:31 +0000

    wip loading the add token spinners with data from the array res files

--- trunk/src/src/uk/co/bitethebullet/android/token/TokenAdd.java
+++ trunk/src/src/uk/co/bitethebullet/android/token/TokenAdd.java
@@ -2,6 +2,8 @@ package uk.co.bitethebullet.android.token;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TokenAdd extends Activity {
 
@@ -10,6 +12,18 @@ public class TokenAdd extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.token_add);
+		
+		loadSpinnerArrayData(R.id.tokenTypeSpinner, R.array.tokenType);
+		loadSpinnerArrayData(R.id.tokenOtpSpinner, R.array.otpLength);
+		loadSpinnerArrayData(R.id.tokenTimeStepSpinner, R.array.timeStep);
+	}
+	
+	
+	private void loadSpinnerArrayData(int spinnerId, int arrayData){
+		Spinner spinner = (Spinner)findViewById(spinnerId);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, arrayData, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 
 }
