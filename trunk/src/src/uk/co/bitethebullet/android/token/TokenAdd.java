61e0968d39c21ea2d895d7fc90614ac9e22df428
diamondz1975 <diamondz1975@c63c96d2-bf12-11de-a250-c7f789e21d83>
2009-11-01 16:58:54 +0000

    WIP add token UI/activity

---
+++ trunk/src/src/uk/co/bitethebullet/android/token/TokenAdd.java
@@ -0,0 +1,15 @@
+package uk.co.bitethebullet.android.token;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TokenAdd extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.token_add);
+	}
+
+}
